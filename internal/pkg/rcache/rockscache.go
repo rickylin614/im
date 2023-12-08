119f69e0c51fb974234955ad9d6197f70f007a7b
rickylin614 <syss8895@gmail.com>
2023-12-08 20:26:36 +0800

    feat: rockscache 預設刪除delay時間

--- internal/pkg/rcache/rockscache.go
+++ internal/pkg/rcache/rockscache.go
@@ -1,6 +1,8 @@
 package rcache
 
 import (
+	"time"
+
 	"github.com/dtm-labs/rockscache"
 	"github.com/redis/go-redis/v9"
 )
@@ -9,6 +11,7 @@ func NewRocksCache(rdb redis.UniversalClient) *rockscache.Client {
 	rcOptions := rockscache.NewDefaultOptions()
 	rcOptions.StrongConsistency = true
 	rcOptions.RandomExpireAdjustment = 0.2
+	rcOptions.Delay = time.Second
 
 	return rockscache.NewClient(rdb, rcOptions)
 }
